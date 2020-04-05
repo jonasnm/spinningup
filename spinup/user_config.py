16cc9f28d279ca9b5ba11be657c9f96fab836507
Christian Jenssen <cjenssen@tuta.io>
2020-04-05 17:25:44 +0200

    *wait_before_launch* set to 0 seconds

--- spinup/user_config.py
+++ spinup/user_config.py
@@ -14,7 +14,8 @@ DEFAULT_BACKEND = {
 }
 
 # Where experiment outputs are saved by default:
-DEFAULT_DATA_DIR = osp.join(osp.abspath(osp.dirname(osp.dirname(__file__))),'data')
+DEFAULT_DATA_DIR = osp.join(osp.abspath(
+    osp.dirname(osp.dirname(__file__))), 'data')
 
 # Whether to automatically insert a date and time stamp into the names of
 # save directories:
@@ -23,6 +24,6 @@ FORCE_DATESTAMP = False
 # Whether GridSearch provides automatically-generated default shorthands:
 DEFAULT_SHORTHAND = True
 
-# Tells the GridSearch how many seconds to pause for before launching 
+# Tells the GridSearch how many seconds to pause for before launching
 # experiments.
-WAIT_BEFORE_LAUNCH = 5
+WAIT_BEFORE_LAUNCH = 0
