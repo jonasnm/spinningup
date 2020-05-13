6da27b0b9f9ccbb71f49f28e2f96ee3de7e37e2d
Christian Jenssen <cjenssen@tuta.io>
2020-05-13 20:48:46 +0200

    Init softmax option-policy

--- spinup/algos/pytorch/soc/soc.py
+++ spinup/algos/pytorch/soc/soc.py
@@ -243,9 +243,10 @@ def soc(env_fn, actor_critic=core.MLPOptionCritic, ac_kwargs=dict(), seed=0,
 
         # values for "beta-target"
         with torch.no_grad():
+            PiO_probs = ac.getOption(o2, ChangeOption=False)
             Qw_next = ac.Qw(o2)
-            V_next = Qw_next.max(-1).values
             Qw_next = Qw_next.gather(-1, w).squeeze(-1)
+            V_next = (PiO_probs*Qw_next).sum(-1)
             Aw = (Qw_next - V_next) + c
 
         # Termination loss
