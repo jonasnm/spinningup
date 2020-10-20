8ec016bd1619c2f902133cf2b6a98bb1ee20c820
Jonas <jonas.n.myhre@uit.no>
2020-10-20 09:23:58 +0200

    Modified setup back to original

--- setup.py
+++ setup.py
@@ -26,9 +26,8 @@ setup(
         'psutil',
         'scipy',
         'seaborn==0.8.1',
-        # 'tensorflow',
-        'torch',
-        'skbuild',
+        'tensorflow>=1.8.0,<2.0',
+        'torch==1.3.1',
         'tqdm'
     ],
     description="Teaching tools for introducing people to deep RL.",
